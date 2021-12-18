05660e0700a6fcdad6590388c9831e020ab68088
Maxime Moreau <contact@maxime-moreau.fr>
2021-12-18 16:10:48 +0100

    improve: install scripts

--- install-scripts/kitty.sh
+++ install-scripts/kitty.sh
@@ -2,5 +2,6 @@
 # your PATH)
 #ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/ \
 # Place the kitty.desktop file somewhere it can be found by the OS
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/ && \
 sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
